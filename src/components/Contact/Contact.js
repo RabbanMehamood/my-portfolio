d0a6b47c7c8b192451111bcdd554ad6331914b19
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-06-27 00:47:47 +0530

    Add Contact, Packages

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+
+
+
+function Contact() {
+    return ( 
+        <div>
+            <h1>To Contact Me, Below are my Links</h1>
+            <div>
+                    <p>Github</p>
+                    <p>LinkdIn</p>
+                    <p>Email: shaik.mehamood786@gmail.com</p>
+            </div>
+        </div>
+     );
+}
+
+export default Contact;
