0c43f0631b43c9764ad57744d4faff5257727d51
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-06-27 22:16:48 +0530

    Add Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AboutMe from './components/AboutMe/AboutMe';
 import Contact from  './components/Contact/Contact';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <AboutMe />
       <Contact />
     </>
